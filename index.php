158fc9a878ede1aae135f9e8d300d6926dd7dc81
butaw45 <kumantheas@gmail.com>
2021-03-29 11:58:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>IoT Relay Test</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+</head>
+
+<body>
+
+</body>
+</html>
