286f74396c475446ad8674e57c4d2035ba62a103
butaw45 <kumantheas@gmail.com>
2021-03-29 13:33:51 +0700

    Baca data jam yg disimpan dari database & Update status saat ini dengan nilai 1

--- cekjam.php
+++ cekjam.php
@@ -2,7 +2,25 @@
 
         //tampilkan jam otomatis
         date_default_timezone_set("Asia/Jakarta");
+        // Format 24jam
         $jam = date('H:i:s');
+        //!Format 12 jam 
+        //!jam = date('h:i:s');
         echo $jam;
+
+        //Koneksi ke database
+        $konek = mysqli_connect("localhost","root","","dbiotrelay");
+
+        //baca data jam yg tersimpan di database
+        $sql = mysqli_query($konek,"SELECT * from jam order by id asc");
+        while($data = mysqli_fetch_array($sql)){
+            $id = $data['id'];
+            $jamdb = $data['jam'];
+            //bandingkan dengan jam saat ini
+        if($jam == $jamdb){
+            //TODO: Update status jam dengan nilai 1
+            mysqli_query($konek, "UPDATE jam set status=1 where id='$id'");
+        }
+        }
 
 ?>
