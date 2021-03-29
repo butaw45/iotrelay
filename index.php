4b5a1da1d8756f5a052b0429f23cff4f104fa765
butaw45 <kumantheas@gmail.com>
2021-03-29 12:03:49 +0700

    Iot Relay Test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>IoT Relay</title>
+  <title>IoT Relay test</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport" />
